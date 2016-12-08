d3af9f3329cb2af4fa2b30f9e9de2c28f1a16a6e
darius94 <little_Darius@columbusstate.edu>
2016-12-08 17:33:22 -0500

    Add files via upload

---
+++ Controllers/DTController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final4125.Controllers
+{
+    public class DTController : Controller
+    {
+        // GET: DT
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Today()
+        {
+            return   "time right now " +  HttpUtility.HtmlEncode(@DateTime.Now.Year) ;
+        }
+    }
+}
---
+++ Models/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Final4125.Models
+{
+   public  class Test
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class TestDBContext : DbContext {
+
+        public DbSet<Test> Movies { get; set; }
+    }
+}
